700f0d9d87c7e327e1fa18e818df09ccfc900b6a
minuk <minukbak@gmail.com>
2020-06-15 07:44:03 +0900

    index 추가

--- routes/page.js
+++ routes/page.js
@@ -2,24 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/profile', (req, res) => {
-    res.render('profile', { title: '내 정보 - dnmblog', user: null });
-});
-
-router.get('/join', (req, res) => {
-    res.render('join', {
-        title: '회원가입 - dnmblog',
-        user: null,
-        joinError: req.flash('joinError'),
-    });
-});
-
-router.get('/', (req, res, next) => {
-    res.render('main', {
-        title: 'dnmblog',
-        twits: [],
-        user: null,
-        loginError: req.flash('loginError'),
+router.get('/', (req,res) => {
+    res.render('index', {
+        title: "DNM-Blog",
     });
 });
 
