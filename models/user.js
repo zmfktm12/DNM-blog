a3a50e1e9d3c1da09f4df8d6d3097c2fb1a8188d
minuk <minukbak@gmail.com>
2020-06-16 21:54:44 +0900

    사용자 테이블 추가

---
+++ models/user.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => (
+    sequelize.define('user', {
+      email: {
+        type: DataTypes.STRING(40),
+        allowNull: true,
+        unique: true,
+      },
+      nick: {
+        type: DataTypes.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+    }, {
+      timestamps: true,
+      paranoid: true,
+    })
+  );
